c790ed5d1440fffe9537aa2ec4f497617206b3e8
brad <brader.wen@gmail.com>
2020-07-21 15:30:03 +0800

    up eth-geth

--- eth-geth/conf/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-nginx
-#openvpn --config /root/CLIENTNAME.ovpn
-#echo "nameserver 8.8.8.8" > /etc/resolv.conf
-#sleep 60
-geth --ipcdisable --cache=512 --rpc --rpcport 8332 --rpcapi "db,eth,net,web3,personal"
-
-
